8c37232bf1ff369fd6f1e49189f99b94992196d8
Andrew <andrew.dremov@gmailcom>
2019-12-12 02:45:55 +0100

    local server works

--- delivery.py
+++ delivery.py
@@ -6,16 +6,18 @@ MOCK_AGENT_URL = "http://mockbcknd.tk/"
 TOM_TOM_API_KEY = "j48lgjJAY1GP2zPa6owAmA3hlia8AImR"
 REQUEST_HEADER = "https://api.tomtom.com/routing/1/calculateRoute/"
 
+
 class Location:
-    def __init__(self, latitude, longitude ):
+    def __init__(self, latitude, longitude):
         self.latitude = latitude
         self.longitude = longitude
 
-    def getLocationsAsString(self):
-        return str(self.latitude)+","+str(self.longitude)
+    def get_locations_as_string(self):
+        return str(self.latitude) + "," + str(self.longitude)
 
     def to_string(self):
-        print ("latitude = ",  self.latitude, "longitude = ", self.longitude)
+        print("latitude = ", self.latitude, "longitude = ", self.longitude)
+
 
 class Agent:
     def __init__(self, location, vehicleType, status):
@@ -24,97 +26,106 @@ class Agent:
         self.status = status
 
     def to_string(self):
-        print( "Location: ", end="")
+        print("Location: ", end="")
         self.location.to_string()
-        print("type: ", self.vehicleType, "status: " , self.status)
+        print("type: ", self.vehicleType, "status: ", self.status)
 
-def vehicleName_to_backendName(name):
-    if(name == "pedestrian"):
+
+def vehicle_name_to_backend_name(name):
+    if name == "pedestrian":
         return "pedestrian"
-    if(name == "EV"):
+    if name == "EV":
         return "car&vehicleEngineType=electric"
-    if(name == "bicycle"):
+    if name == "bicycle":
         return "bicycle"
-    if(name == "e-bike"):
+    if name == "e-bike":
         return "bicycle&vehicleEngineType=electric"
-    if( name == "auto"):
+    if name == "auto":
         return "car"
 
 
 #### test code
 # Make the Request (Dont foreget to change the API key)
-#response = requests.get("https://api.tomtom.com/routing/1/calculateRoute/37.773412,-122.430929:37.787402,-122.396555/xml?avoid=unpavedRoads&key=j48lgjJAY1GP2zPa6owAmA3hlia8AImR")
+# response = requests.get("https://api.tomtom.com/routing/1/calculateRoute/37.773412,-122.430929:37.787402,-122.396555/xml?avoid=unpavedRoads&key=j48lgjJAY1GP2zPa6owAmA3hlia8AImR")
 # Print out the response to make sure it went through
-#print(response.content)
+# print(response.content)
 
-
-def calculateBestRoutes(start , finish, listOfAgents):
-    startLocString = start.getLocationsAsString()
-    finishLocString = finish.getLocationsAsString()
+def calculate_best_routes(start, finish, listOfAgents):
+    startLocString = start.get_locations_as_string()
+    finishLocString = finish.get_locations_as_string()
 
     jsonResponces = []
     for freeAgent in listOfAgents:
         requestStr = REQUEST_HEADER
-        requestStr += freeAgent.location.getLocationsAsString() +":"
-        requestStr += startLocString +":"
+        requestStr += freeAgent.location.get_locations_as_string() + ":"
+        requestStr += startLocString + ":"
         requestStr += finishLocString
         requestStr += "/json?maxAlternatives=1&traffic=true&avoid=unpavedRoads&travelMode="
         requestStr += freeAgent.vehicleType
         requestStr += "&key="
         requestStr += TOM_TOM_API_KEY
-        #print ("Request: ", requestStr)
+        # print ("Request: ", requestStr)
         response = requests.get(requestStr)
         jsonResponces.append(response.content)
-        #print (response.content)    
+        # print (response.content)
 
     # return list with best routes in JSON. 
     return jsonResponces
 
-def parseAgents(data):
+
+def parse_agents(data):
     agents = []
-    jsonAgents = data["features"]
-    #print (jsonAgents)
-    for dictAgent in jsonAgents:
-        #print (dictAgent)
+    json_agents = data["features"]
+    # print (json_agents)
+    for dictAgent in json_agents:
+        # print (dictAgent)
         agentProperties = dictAgent["properties"]
-        #print (agentProperties)
-        vehicleId = vehicleName_to_backendName(agentProperties["vehicleType"])
+        # print (agentProperties)
+        vehicleId = vehicle_name_to_backend_name(agentProperties["vehicleType"])
         status = agentProperties["status"]
         agentCoords = dictAgent["geometry"]["coordinates"]
-        newAgent = Agent(Location(agentCoords[1], agentCoords[0]),vehicleId,status)
+        newAgent = Agent(Location(agentCoords[1], agentCoords[0]), vehicleId, status)
         agents.append(newAgent)
 
     return agents
 
-def getFreeAgents(agents):
+
+def get_free_agents(agents):
     freeAgents = []
 
     for agent in agents:
-        if( agent.status == "Free"):
+        if agent.status == "Free":
             freeAgents.append(agent)
 
     return freeAgents
 
-#1. Read agents and parse to list
+
+# 1. Read agents and parse to list
 mockAgents = requests.get(MOCK_AGENT_URL)
 data = json.loads(mockAgents.content)
-agentsList = parseAgents(data)
+agentsList = parse_agents(data)
 # pick only free agents
-freeAgents = getFreeAgents(agentsList)
-print ("We have ",len(freeAgents)," free agents")
+freeAgents = get_free_agents(agentsList)
+print("We have ", len(freeAgents), " free agents")
 for agent in freeAgents:
     agent.to_string()
 
-if(len(freeAgents) == 0):
-    print ("Unfortunatelly no free courier")
+if len(freeAgents) == 0:
+    print("Unfortunatelly no free courier")
     exit()
 
-#2. fake start point and finish point
-startLocation = Location(52.522156, 13.411048) # fake Alexander Platz
-finishLocation = Location(52.516321, 13.377549) # fake Brandenburg Tor
+# 2. fake start point and finish point
+startLocation = Location(52.522156, 13.411048)  # fake Alexander Platz
+finishLocation = Location(52.506321, 13.277549)  # fake Brandenburg Tor
 
-#3. calculate best routes
-bestRoutes = calculateBestRoutes(startLocation, finishLocation, freeAgents)
+
+# 3. calculate best routes
+def get_best_routes():
+    best_routes = calculate_best_routes(startLocation, finishLocation, freeAgents)
+    return best_routes
 
 
 # TODO  provide best rotes to frontend
+mockAgents = requests.get("http://mockbcknd.tk/")
+# print(mockAgents.content)
+print(get_best_routes())
